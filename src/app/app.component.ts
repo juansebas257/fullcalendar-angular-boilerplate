4a1cec01474519f023800cdd9d2445e5745d8051
Sebastián Jiménez <juansebas257@gmail.com>
2021-04-03 22:37:47 -0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CalendarOptions } from '@fullcalendar/common';
+import esLocale from '@fullcalendar/core/locales/es';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'fullcalendar';
+
+  calendarOptions: CalendarOptions = {
+    initialView: 'timeGridWeek',
+    locale: esLocale,
+    aspectRatio: 2,
+    selectable: true,
+    editable: true,
+    headerToolbar: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+    },
+    dateClick: this.dateClick.bind(this),
+    select: this.select.bind(this),
+    eventDrop: this.eventDrop.bind(this),
+    eventResize: this.eventResize.bind(this),
+    events: [
+      { id: '1', title: 'event 1', start: '2021-04-01 08:00', end: '2021-04-01 10:00' },
+      { id: '2', title: 'event 2', start: '2021-04-02 09:00', end: '2021-04-01 11:00' },
+      { id: '3', title: 'event 3', start: '2021-04-01 14:00', end: '2021-04-01 14:30' },
+    ]
+  };
+
+  dateClick(arg: any) {
+    console.log('date click: ', arg)
+  }
+
+
+  select(arg: any) {
+    console.log('select: ', arg)
+  }
+  eventDrop(arg: any) {
+    console.log('event drop: ', arg)
+  }
+  eventResize(arg: any) {
+    console.log('event resized: ', arg)
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FullCalendarModule } from '@fullcalendar/angular';
 import { AppComponent } from './app.component';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
+import listPlugin from '@fullcalendar/list';
+
+//import interactionPlugin from '@fullcalendar/interaction'; // a plugin
+
+FullCalendarModule.registerPlugins([ // register FullCalendar plugins
+  dayGridPlugin,
+  timeGridPlugin,
+  listPlugin,
+  interactionPlugin
+]);
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FullCalendarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
